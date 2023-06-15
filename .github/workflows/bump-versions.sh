b72f1d9137a5c0cd476cc69121fba5f646ef635c
Andrei Pohilko <andrei.pokhilko@gmail.com>
2023-06-15 10:22:14 +0100

    Remove unused CI code

--- .github/workflows/bump-versions.sh
+++ .github/workflows/bump-versions.sh
@@ -9,7 +9,6 @@ WORKING_DIRECTORY="$PWD"
 }
 
 sed -i -e "s/appVersion.*/appVersion: \"${APP_VERSION}\" /g" ${HELM_CHARTS_SOURCE}/Chart.yaml
-sed -i -e "s/version.*/version: \"${APP_VERSION}\" /g" plugin.yaml
 CURRENT_VERSION=$(cat ${HELM_CHARTS_SOURCE}/Chart.yaml | grep 'version:' | awk '{print $2}')
 NEW_VERSION=$(echo $CURRENT_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 sed -i -e "s/${CURRENT_VERSION}/${NEW_VERSION}/g" ${HELM_CHARTS_SOURCE}/Chart.yaml
