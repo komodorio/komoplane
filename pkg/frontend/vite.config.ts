bfe57dfc36c4bbcdf97838b1e07ef9717bfaa3e3
Andrei Pohilko <andrei.pokhilko@gmail.com>
2023-08-23 13:39:35 +0100

    Generate sourcemaps

--- pkg/frontend/vite.config.ts
+++ pkg/frontend/vite.config.ts
@@ -5,4 +5,7 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), pluginRewriteAll()],
+    build: {
+        sourcemap: true,
+    },
 })
