e04d6ecdbd0d9729d37b2d11a17024bbe566f8d9
Andrey Pokhilko <andrei.pokhilko@gmail.com>
2024-08-14 11:04:39 +0100

    Better pluralizer (#66)
    * Better pluralizer
    
    * fix missing case
    
    * Bump up golang
    
    * Bump up some libs
    
    * Bump up crossplane
    
    * Revert modifications
    
    * Update goreleaser flags

--- pkg/backend/utils/utils.go
+++ pkg/backend/utils/utils.go
@@ -2,6 +2,19 @@ package utils
 
 import "strings"
 
-func Plural(resName string) string {
-	return strings.ToLower(resName) + "s" // TODO: is this the best way to obtain endpoint name?
+func Plural(resName string) string { // TODO: is this the best way to obtain endpoint name?
+	noun := strings.ToLower(resName)
+	if strings.HasSuffix(noun, "s") || strings.HasSuffix(noun, "x") || strings.HasSuffix(noun, "z") || strings.HasSuffix(noun, "sh") || strings.HasSuffix(noun, "ch") {
+		return noun + "es"
+	} else if strings.HasSuffix(noun, "y") && !strings.HasSuffix(noun, "ay") && !strings.HasSuffix(noun, "ey") && !strings.HasSuffix(noun, "iy") && !strings.HasSuffix(noun, "oy") && !strings.HasSuffix(noun, "uy") {
+		return noun[:len(noun)-1] + "ies"
+	} else if strings.HasSuffix(noun, "f") {
+		return noun[:len(noun)-1] + "ves"
+	} else if strings.HasSuffix(noun, "fe") {
+		return noun[:len(noun)-2] + "ves"
+	} else if strings.HasSuffix(noun, "o") {
+		return noun + "es"
+	} else {
+		return noun + "s"
+	}
 }
