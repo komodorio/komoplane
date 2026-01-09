54c4c5db671a21b249f293d157182345f99a5533
Andrei Pohilko <andrei.pokhilko@gmail.com>
2026-01-09 17:20:02 +0000

    Remove plugin

--- pkg/frontend/vite.config.ts
+++ pkg/frontend/vite.config.ts
@@ -1,10 +1,9 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), pluginRewriteAll()],
+    plugins: [react()],
     build: {
         sourcemap: true,
     },
